c8f9db5bbc10df1e92ade1b4d9ca17cf7ccab1e8
Manoel <120431500+Raeritz@users.noreply.github.com>
2025-03-13 00:47:16 +0000

    Create Maior.py

---
+++ Faculdade/Programacao-de-Computadores/2024.1-PC-Lista-03/Maior-de-2-inteiros.py
@@ -0,0 +1,14 @@
+# Escreva um programa que leia 2 (dois) números inteiros a e b e mostre o maior dos 2.
+
+# Input
+# A entrada é composta por duas linhas. A primeira linha contém apenas um inteiro a (1 ≤ a ≤ 109) e a segunda um inteiro b (1 ≤ b ≤ 109).
+# Output
+# Seu programa deve mostrar uma única linha contendo o valor do maior inteiro.
+
+a = input()
+b = input()
+
+if(a >= b):
+    print(a)
+else:
+    print(b)
